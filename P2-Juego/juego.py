3fe0fc984ab4d891e388276c5e9a692e7933c8c0
Ruben <99rubenche@gmail.com>
2019-09-15 22:51:49 +0200

    juego funcional, sol bugs

--- P2-Juego/juego.py
+++ P2-Juego/juego.py
@@ -1,35 +1,34 @@
 
-import random 
+import random
 
 ## Clase Jugador
 class Jugador(object):
 
 	def __init__(self, name, tipo, numPensado):
 		super(Jugador, self).__init__()
-		self.arg = arg
-		self.name = "name"
+		self.name = name
 		self.tipo = tipo
 		self.numPensado = numPensado
 
 	def pensarNumero(self):
 		if(self.tipo == "Humano"):
-			self.numpensado = int(input("[+] Piense y introduzca un numero: "))
+			self.numPensado = int(input("[+] %s piense e introduzca un numero: " % self.name))
 
 	def proponerNumero(self):
 		if(self.tipo == "Humano"):
-			numPropuesto = int(input("Introduzca un numero: "))
+			return int(input("[+] %s introduzca un numero: " % self.name))
 		else:
-			numPropuesto = random.randint(1, 101)
+			return random.randint(1, 10)
 
 	def comprNumero(self, num):
 		if (self.numPensado > num):
-			print("El numero es mayor")
+			print("[+] El numero pensado es mayor")
 			return False
 		elif (self.numPensado < num):
-			print("El numero es menor")
+			print("[+] El numero pensado es menor")
 			return False
 		else:
-			print("Has acertado el numero!")
+			print("[+] Has acertado el numero!" % self.name)
 			return True
 
 
@@ -38,55 +37,54 @@ class Partida(object):
 
 	def __init__(self, jugador1, jugador2):
 		super(Partida, self).__init__()
-		self.arg = arg
-		self.jugador1 = jugador1
-		self.jugador2 = jugador2
+		self.j1 = jugador1
+		self.j2 = jugador2
 		self.numIntentosJ1 = 0
 		self.numIntentosJ2 = 0
 
 	def jugar(self):
 
-
-
-		
 		self.j1.pensarNumero()
-		while(True):
-			if(j2.comprNumero(j1.proponerNumero() == False)):
-				numIntentosJ1 += 1
+		while (True):
+			if (self.j1.comprNumero(self.j2.proponerNumero()) == False):
+				self.numIntentosJ2 += 1
 			else:
-				print("El jugador numero 1 ha ganado!")
+				print("[+] %s ha realizado %d intentos." % ( self.j2.name, self.numIntentosJ2))
 				break
 
 		self.j2.pensarNumero()
-		while(True):
-			if(j2.comprNumero(j1.proponerNumero() == False)):
-				numIntentosJ1 += 1
+		while (True):
+			if (self.j2.comprNumero(self.j1.proponerNumero()) == False):
+				self.numIntentosJ1 += 1
 			else:
-				print("El jugador numero 1 ha ganado!")
+				print("[+] %s ha realizado %d intentos." % (self.j1.name, self.numIntentosJ1))
 				break
 
-		if(numIntentosJ1 > numIntentosJ2):
-			print("El jugador 1 ha ganado la partida")
-		elif(numIntentosJ1 < numIntentosJ2):
-			print("El jugador 1 ha ganado la partida")
-		else 
-			print("Empate")
+		print("[+] La partida ha finalizado.")
+		if (self.numIntentosJ1 > self.numIntentosJ2):
+			print("[+] %s ha ganado la partida" % self.j2.name)
+		elif (self.numIntentosJ1 < self.numIntentosJ2):
+			print("[+] El jugador 1 ha ganado la partida" % self.j1.name)
+		else: 
+			print("[+] Empate")
 		
 
 def main():
 	
-	print("Jugador1: ")
-	name = str(input("\tIndica un nombre para el jugador 1: "))
-	tipo = str(input("\tIndica maquina/humano : "))
-	j1 = Jugador(name, tipo, random.randint(1, 101))
-	
-	print("Jugador2: ")
-	name = str(input("\tIndica un nombre: "))
-	tipo = str(input("\tIndica maquina/humano : "))
-	j2 = Jugador(name, tipo, random.randint(1, 101))
+	print("[+] Jugador 1: ")
+	name = str(input("\t- Indica un nombre para el jugador 1: "))
+	tipo = str(input("\t- Indica maquina/humano : "))
+	j1 = Jugador(name, tipo, random.randint(1, 10))
+
+	print("Jugador 2: ")
+	name = str(input("\t- Indica un nombre: "))
+	tipo = str(input("\t- Indica maquina/humano : "))
+	j2 = Jugador(name, tipo, random.randint(1, 10))
 
 	# Evito pasar numero intentos por argumento pork el valor de ambos es de 0 al inicio
-	p = Partida(j1, j2, 0, 0)
+	p = Partida(j1, j2)
+
+	p.jugar()
 
 
 if __name__ == '__main__':
