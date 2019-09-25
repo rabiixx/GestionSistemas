aa6313b9b9270b2f7b52e0fa6ab057ba76fdd9d9
rabiixx <rabiixx@gmail.com>
2019-09-25 18:06:31 +0200

    updated p1- juego_V2

---
+++ practica1/juego_V2.py
@@ -0,0 +1,114 @@
+
+import random
+import os
+
+## Clase Jugador
+class Jugador(object):
+
+
+
+	def __init__(self, name, tipo, numPensado, numPropuesto, res):
+		super(Jugador, self).__init__()
+		self.name = name
+		self.tipo = tipo
+		self.numPensado = numPensado
+		self.numPropuesto = numPropuesto 
+		self.res = res
+
+
+	#def pensarNumero(self):
+		#self.numPensado = int(input("[+] %s piense e introduzca un numero: " % self.name))
+
+	def proponerNumero(self):
+		
+		if(self.tipo == "Humano"):
+			return int(input("[+] %s introduzca un numero: " % self.name))
+		else:
+
+			if ( (res == 'Mayor') or (res == 'mayor') ):
+				self.numPropuesto = random.randint(self.numPropuesto, 10)
+			elif ( (res == 'Menor') or (res == 'menor') ):
+				self.numPropuesto = random.randint(1, self.numPropuesto)
+			else:
+				self.numPropuesto = random.randint(1, 10)
+
+			print("El número que has pensado es el %d" %self.numPropuesto)
+
+	def comprNumero(self, num):
+		
+		if (self.tipo == 'Maquina'):
+			if (self.numPensado > num):
+				print("[+] El numero pensado es mayor")
+				return False
+			elif (self.numPensado < num):
+				print("[+] El numero pensado es menor")
+				return False
+			else:
+				print("[+] %s Has acertado el numero!" % self.name)
+				return True
+		else:
+			return input("Mayor/Menor/Correcto?: ")
+
+## Clase Partida
+class Partida(object):
+
+	def __init__(self, jugador1, jugador2):
+		super(Partida, self).__init__()
+		self.j1 = jugador1
+		self.j2 = jugador2
+
+	def jugar(self):
+
+		numIntentosJ1 = 0;
+		numIntentosJ2 = 0;
+
+		print("Bueno, %s, estoy pensando en un número entre 1 y 100. Intenta adivinarlo." %self.j1.name)
+
+		while (self.j2.comprNumero(self.j1.proponerNumero()) == False):
+			numIntentosJ1 += 1
+		
+		print("[+] ¡Buen trabajo, %s ¡Has adivinado mi número en %d intentos! Es tu turno." % (self.j1.name, self.numIntentosJ1))
+
+		#self.j2.pensarNumero()
+		
+		while ( (self.j2.res != 'Correcto') or (self.j2.res != 'correcto')):
+			self.j2.res = self.j1.comprNumero(self.j2.proponerNumero())
+			numIntentosJ2 += 1
+		
+		print("[+] La partida ha finalizado.")
+		print("[+] Numero intentos: ")
+		print("\t- %s: %d" % (self.j1.name, numIntentosJ1))
+		print("\t- Yo: %d\n" %numIntentosJ2)
+
+		if (numIntentosJ1 > numIntentosJ2):
+			return input("[+] Yo gano. He acertado en %d intentos. Quieres seguir jugando? [S/N]: " % numIntentosJ2)
+		elif (numIntentosJ1 < numIntentosJ2):
+			return input("[+] Tu ganas. Has acertado en %d intentos. Quieres seguir jugando? [S/N]: " % numIntentosJ1)
+		else: 
+			print("[+] Empate. Quieres seguir jugando? [S/N]: ")
+		
+
+def main():
+	
+	os.system("clear");
+	print("[+] Bienvenido")
+	name = str(input("\t[+] !Hola¡ ¿Como te llamas?: "))
+	j1 = Jugador(name, "Humano", 0, 0, ' ')
+	j2 = Jugador("BuggedBot", "Maquina", random.randint(1, 10), 0 , ' ')
+
+	# Evito pasar numero intentos por argumento pork el valor de ambos es de 0 al inicio
+	p = Partida(j1, j2)
+	p.jugar()
+
+	'''
+	while(p.jugar() == 'S'):
+		if(input("Deseas cambiarte de nombre? [S/N]: ") == 'S'):
+			name = input("Introduzca el nuevo nombre: ")
+			j1 = Jugador(name, "Humano", 0)
+			p.jugar()
+		else:
+			p.jugar()
+	'''
+
+if __name__ == '__main__':
+	main()
