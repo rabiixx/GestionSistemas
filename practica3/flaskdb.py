461fbb1487bdec2e44aa5d216a5fa0a38e4337c3
Ruben <99rubenche@gmail.com>
2019-10-04 10:47:54 +0200

    tdg

--- practica3/flaskdb.py
+++ practica3/flaskdb.py
@@ -4,7 +4,6 @@ import json
 
 app = Flask(__name__)
 
-
 @app.route("/")
 def home():
 	return render_template("home.html")
@@ -13,27 +12,38 @@ def home():
 def about():
 	return render_template("about.html")
 
-@app.route("/<tabla>")
-def connect_db():
+@app.route("/tablas/")
+def query1(tabla):
 	conn = sqlite3.connect("ejemplo.db") 
 	c = conn.cursor() 
-	query  = "SELECT * FROM {}".format(tabla)
+	query  = "select * from sqlite_master where type='table';"
+	c.execute(query)
+	res = c.fetchall()
+	return json.dumps(res)
+
+@app.route("(tablas/<tabla>/")
+def query2(tabla):
+	conn = sqlite3.connect("ejemplo.db") 
+	c = conn.cursor() 
+	query  = "SELECT * FROM " + str(tabla)
+	print(query)
 	#query = "SELECT * FROM Clientes"
 	c.execute(query)
 	res = c.fetchall()
 	return json.dumps(res)
 	#return render_template("tablas.html")
-'''
-@app.route("/tablas/<IdCliente>")
-def connect_db():
+
+@app.route("(tablas/<tabla>/<campo>")
+def query3(tabla, campo):
 	conn = sqlite3.connect("ejemplo.db") 
 	c = conn.cursor() 
-	
-	query = ("SELECT * FROM Clientes")
+	query  = "SELECT " + campo + " FROM " + tabla
+	print(query)
+	#query = "SELECT * FROM Clientes"
 	c.execute(query)
 	res = c.fetchall()
-	json.dumps(res)
-	return render_template("tablas.html")
+	return json.dumps(res)
+	#return render_template("tablas.html")
 '''
 if __name__ == "__main__":
 	app.run()
